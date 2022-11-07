e7aa9f5ee001beb98b481dc4693beb0c38959154
sanek200000 <heist2000@gmail.com>
2022-11-07 23:25:23 +0300

    add lesson1 & lesson2

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lesson_one',
+    'lesson_two',
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from lesson_two import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    #path(r'', include('lesson_one.urls')),
+    path(r'', views.home),
 ]
